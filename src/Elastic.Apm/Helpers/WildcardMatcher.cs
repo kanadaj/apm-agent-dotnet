28e7df0031f9db1b6b7c4aafb837293fb03c42b4
Greg Kalapos <gergo@kalapos.net>
2019-10-08 11:57:24 +0200

    Update src/Elastic.Apm/Helpers/WildcardMatcher.cs

--- src/Elastic.Apm/Helpers/WildcardMatcher.cs
+++ src/Elastic.Apm/Helpers/WildcardMatcher.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Elastic.Apm.Helpers
 {
 	/// <summary>
+	/// Heavily inspired by the Java Elastic APM Agent.
 	/// Wildcard matcher to e.g. sanitize strings.
 	/// Examples: `/foo/*/bar/*/baz*`, `*foo*`.
 	/// Matching is case insensitive by default.
@@ -13,11 +13,11 @@ namespace Elastic.Apm.Helpers
 	/// </summary>
 	internal abstract class WildcardMatcher
 	{
-		private static readonly string CASE_INSENSITIVE_PREFIX = "(?i)";
+		private const string CaseInsensitivePrefix = "(?i)";
 
-		private static readonly string CASE_SENSITIVE_PREFIX = "(?-i)";
+		private const string CaseSensitivePrefix = "(?-i)";
 
-		private static readonly string WILDCARD = "*";
+		private const string Wildcard = "*";
 
 		public abstract string GetMatcher();
 
@@ -28,51 +28,39 @@ namespace Elastic.Apm.Helpers
 		/// <returns>Whether the string matches the given pattern</returns>
 		public abstract bool Matches(string s);
 
-		/**
-		 * This is a different version of {@link #matches(String)} which has the same semantics as calling
-		 * {@code matcher.matches(firstPart + secondPart);}.
-		 * <p>
-		 * The difference is that this method does not allocate memory.
-		 * </p>
-		 *
-		 * @param firstPart  The first part of the string to match against.
-		 * @param secondPart The second part of the string to match against.
-		 * @return {@code true},
-		 * when the wildcard pattern matches the partitioned string,
-		 * {@code false} otherwise.
-		 */
+		/// <summary>
+		/// This is a different version of <see cref="Matches(string)"/> which has the same semantics as calling
+		/// <see cref="Matches(string)" param=" firstPart + secondPart" />.
+		/// The difference is that this method does not allocate memory.
+		/// </summary>
+		/// <param name="firstPart">The first part of the string to match against.</param>
+		/// <param name="secondPart">The second part of the string to match against.</param>
+		/// <returns><code>true</code> when the wildcard pattern matches the partitioned string, <code>false</code> otherwise. </returns>
 		protected abstract bool Matches(string firstPart, string secondPart);
 
-		/**
-		 * Constructs a new {@link WildcardMatcher} via a wildcard string.
-		 * <p>
-		 * It supports the {@code *} wildcard which matches zero or more characters.
-		 * </p>
-		 * <p>
-		 * By default, matches are a case insensitive.
-		 * Prepend {@code (?-i)} to your pattern to make it case sensitive.
-		 * Example: {@code (?-i)foo*} matches the string {@code foobar} but does not match {@code FOOBAR}.
-		 * </p>
-		 * <p>
-		 * It does NOT support single character wildcards like {@code f?o}
-		 * </p>
-		 *
-		 * @param wildcardString The wildcard string.
-		 * @return The {@link WildcardMatcher}
-		 */
+		/// <summary>
+		/// Constructs a new <see cref="WildcardMatcher"/> via a wildcard string.
+		/// By default, matches are a case insensitive.
+		/// It supports the <code>*</code> wildcard which matches zero or more characters.
+		/// Prepend <code>(?-i)</code> to your pattern to make it case sensitive.
+		///
+		/// Example: <code>(?-i)foo*</code> matches the string <code>foobar</code> but does not match <code>FOOBAR</code>.
+		/// </summary>
+		/// <param name="wildcardString"></param>
+		/// <returns></returns>
 		public static WildcardMatcher ValueOf(string wildcardString)
 		{
 			var matcher = wildcardString;
 			var ignoreCase = true;
-			if (matcher.StartsWith(CASE_SENSITIVE_PREFIX))
+			if (matcher.StartsWith(CaseSensitivePrefix))
 			{
 				ignoreCase = false;
-				matcher = matcher.Substring(CASE_SENSITIVE_PREFIX.Length);
+				matcher = matcher.Substring(CaseSensitivePrefix.Length);
 			}
-			else if (matcher.StartsWith(CASE_INSENSITIVE_PREFIX)) matcher = matcher.Substring(CASE_INSENSITIVE_PREFIX.Length);
+			else if (matcher.StartsWith(CaseInsensitivePrefix)) matcher = matcher.Substring(CaseInsensitivePrefix.Length);
 
 			var split = matcher.Split('*');
-			if (split.Length == 1) return new SimpleWildcardMatcher(split[0], matcher.StartsWith(WILDCARD), matcher.EndsWith(WILDCARD), ignoreCase);
+			if (split.Length == 1) return new SimpleWildcardMatcher(split[0], matcher.StartsWith(Wildcard), matcher.EndsWith(Wildcard), ignoreCase);
 
 			var matchers = new List<SimpleWildcardMatcher>(split.Length);
 			for (var i = 0; i < split.Length; i++)
@@ -80,50 +68,44 @@ namespace Elastic.Apm.Helpers
 				var isFirst = i == 0;
 				var isLast = i == split.Length - 1;
 				matchers.Add(new SimpleWildcardMatcher(split[i],
-					!isFirst || matcher.StartsWith(WILDCARD),
-					!isLast || matcher.EndsWith(WILDCARD),
+					!isFirst || matcher.StartsWith(Wildcard),
+					!isLast || matcher.EndsWith(Wildcard),
 					ignoreCase));
 			}
-			return new CompoundWildcardMatcher(wildcardString, matcher, matchers);
+			return new CompoundWildcardMatcher(matcher, matchers);
 		}
 
-		/**
-		 * Returns the first {@link WildcardMatcher} {@linkplain WildcardMatcher#matches(String) matching} the provided string.
-		 *
-		 * @param matchers the matchers which should be used to match the provided string
-		 * @param s        the string to match against
-		 * @return the first matching {@link WildcardMatcher}, or {@code null} if none match.
-		 */
+
+		/// <summary>
+		/// Returns <code>true</code>, if any of the matchers match the provided string.
+		/// </summary>
+		/// <param name="matchers">The matchers which should be used to match the provided string</param>
+		/// <param name="s">The string to match against</param>
+		/// <returns><code>true</code>, if any of the matchers match the provided string</returns>
 		public static bool IsAnyMatch(List<WildcardMatcher> matchers, string s)
 		{
 			return AnyMatch(matchers, s) != null;
 		}
 
-		/**
-		 * Returns {@code true}, if any of the matchers match the provided string.
-		 *
-		 * @param matchers the matchers which should be used to match the provided string
-		 * @param s        the string to match against
-		 * @return {@code true}, if any of the matchers match the provided string
-		 */
-
+		/// <summary>
+		/// Returns the first <see cref="WildcardMatcher"/> that matches the provided string.
+		/// </summary>
+		/// <param name="matchers"> The matchers which should be used to match the provided string</param>
+		/// <param name="s">The string to match against</param>
+		/// <returns>The first matching <see cref="WildcardMatcher"/>, or <code>null</code> if none match.</returns>
 		private static WildcardMatcher AnyMatch(List<WildcardMatcher> matchers, string s)
 		{
-			if (s == null) return null;
-
-			return AnyMatch(matchers, s, null);
+			return s == null ? null : AnyMatch(matchers, s, null);
 		}
 
-		/**
-		 * Returns the first {@link WildcardMatcher} {@linkplain WildcardMatcher#matches(String) matching} the provided partitioned string.
-		 *
-		 * @param matchers   the matchers which should be used to match the provided string
-		 * @param firstPart  The first part of the string to match against.
-		 * @param secondPart The second part of the string to match against.
-		 * @return the first matching {@link WildcardMatcher}, or {@code null} if none match.
-		 * @see #matches(String, String)
-		 */
-		private static WildcardMatcher AnyMatch(List<WildcardMatcher> matchers, string firstPart, string secondPart)
+		/// <summary>
+		/// Returns the first <see cref="WildcardMatcher"/> that matches the provided partitioned string.
+		/// </summary>
+		/// <param name="matchers"> The matchers which should be used to match the provided string</param>
+		/// <param name="firstPart"> The first part of the string to match against.</param>
+		/// <param name="secondPart"> The second part of the string to match against.</param>
+		/// <returns>The first matching <see cref="WildcardMatcher"/>, or <code>null</code> if none match.</returns>
+		private static WildcardMatcher AnyMatch(IReadOnlyCollection<WildcardMatcher> matchers, string firstPart, string secondPart)
 		{
 			for (var i = 0; i < matchers.Count; i++)
 			{
@@ -134,10 +116,17 @@ namespace Elastic.Apm.Helpers
 			return null;
 		}
 
-		/*
-		 * Based on https://stackoverflow.com/a/29809553/1125055
-		 * Thx to Zach Vorhies
-		 */
+		/// <summary>
+		/// Based on https://stackoverflow.com/a/29809553/1125055
+		/// Thx to Zach Vorhies
+		/// </summary>
+		/// <param name="haystack1"></param>
+		/// <param name="haystack2"></param>
+		/// <param name="needle"></param>
+		/// <param name="ignoreCase"></param>
+		/// <param name="start"></param>
+		/// <param name="end"></param>
+		/// <returns></returns>
 		private static int IndexOfIgnoreCase(string haystack1, string haystack2, string needle, bool ignoreCase, int start, int end)
 		{
 			if (start < 0) return -1;
@@ -183,23 +172,20 @@ namespace Elastic.Apm.Helpers
 			return i < firstPartLength ? firstPart.ElementAt(i) : secondPart.ElementAt(i - firstPartLength);
 		}
 
-		/**
-		 * This {@link WildcardMatcher} supports wildcards in the middle of the matcher by decomposing the matcher into several
-		 * {@link SimpleWildcardMatcher}s.
-		 */
+		/// <summary>
+		/// This <see cref="WildcardMatcher"/> supports wildcards in the middle of the matcher by decomposing the matcher into several
+		/// <see cref="WildcardMatcher.SimpleWildcardMatcher"/>.
+		/// </summary>
 		private class CompoundWildcardMatcher : WildcardMatcher
 		{
 			private readonly string _matcher;
 			private readonly List<SimpleWildcardMatcher> _wildcardMatchers;
 
-			public CompoundWildcardMatcher(string wildcardString, string matcher, List<SimpleWildcardMatcher> wildcardMatchers)
+			public CompoundWildcardMatcher(string matcher, List<SimpleWildcardMatcher> wildcardMatchers)
 			{
-				_wildcardString = wildcardString;
 				_matcher = matcher;
 				_wildcardMatchers = wildcardMatchers;
 			}
-
-			private string _wildcardString;
 
 			public override bool Matches(string s)
 			{
@@ -235,9 +221,9 @@ namespace Elastic.Apm.Helpers
 			}
 		}
 
-		/**
-		 * This {@link} does not support wildcards in the middle of a matcher.
-		 */
+		/// <summary>
+		/// This <see cref="WildcardMatcher"/> does not support wildcards in the middle of a matcher.
+		/// </summary>
 		private class SimpleWildcardMatcher : WildcardMatcher
 		{
 			public readonly string Matcher;
@@ -254,16 +240,14 @@ namespace Elastic.Apm.Helpers
 				_wildcardAtEnd = wildcardAtEnd;
 				_wildcardAtBeginning = wildcardAtBeginning;
 				_ignoreCase = ignoreCase;
-				_stringRepresentation =
-					new StringBuilder(matcher.Length + CASE_SENSITIVE_PREFIX.Length + WILDCARD.Length + WILDCARD.Length)
-						.Append(ignoreCase ? "" : CASE_SENSITIVE_PREFIX)
-						.Append(wildcardAtBeginning ? WILDCARD : "")
-						.Append(matcher)
-						.Append(wildcardAtEnd ? WILDCARD : "")
-						.ToString();
+//				StringRepresentation =
+//					new StringBuilder(matcher.Length + CaseSensitivePrefix.Length + Wildcard.Length + Wildcard.Length)
+//						.Append(ignoreCase ? "" : CaseSensitivePrefix)
+//						.Append(wildcardAtBeginning ? Wildcard : "")
+//						.Append(matcher)
+//						.Append(wildcardAtEnd ? Wildcard : "")
+//						.ToString();
 			}
-
-			private string _stringRepresentation;
 
 			public override bool Matches(string s)
 			{
